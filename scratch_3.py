3ab37f2f981604f16670a522dddfb295b5382407
alperenkavukk <80036024+alperenkavukk@users.noreply.github.com>
2021-06-28 14:50:50 +0300

    Add files via upload
    yemekhane sistemi

---
+++ scratch_3.py
@@ -0,0 +1,27 @@
+print("Yemekhane Sistemine Hoşgeldiniz!")
+yemeklistesi=[];yemeksozlugu={}
+gun="";anayemek="";arasicak="";Corba="";SalataveyaTatli="";aranilanyemek=""
+while(True):
+   secim=int(input("seçiminizi yapınız"))
+   if 1 == secim:
+       gun=input("Gün Adını Giriniz")
+       anayemek=input("Ana Yemeği Giriniz")
+       yemeklistesi.append(anayemek)
+       arasicak=input("AraSıcak Yemeği Giriniz")
+       Corba=input("Çorba Adını Yazınız")
+       SalataveyaTatli=input("Salata veya Tatlıyı Yazınız")
+
+       yemeksozlugu.setdefault(gun, yemeklistesi)
+   elif 2== secim:
+       sayac = 0
+       aranilanyemek = input("Aradığınız Yemeği Giriniz")
+       yemeksozlugu.setdefault(gun, yemeklistesi)
+       for i in yemeklistesi:
+           if (aranilanyemek):
+               sayac += 1
+               yazi1 = "{} yemeği {} defa yenilmiştir."
+               print(yazi1.format(aranilanyemek, sayac))
+           else:
+                break;
+
+
